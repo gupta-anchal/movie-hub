08967dee48d78e06cb247439e2b1bb935e637a14
w3villa-rahul-yadav <rahul.yadav@w3villa.com>
2023-12-28 07:42:18 +0530

    added pagination on movie list page

--- src/app/(routes)/movielist/page.jsx
+++ src/app/(routes)/movielist/page.jsx
@@ -1,5 +1,8 @@
 "use client";
+import Image from "next/image";
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
 
 const Movielist = () => {
   const [movies, setMovies] = useState([]);
@@ -7,9 +10,10 @@ const Movielist = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await fetch("/api/movies/list"); 
+        const response = await fetch("/api/movies/list");
         const data = await response.json();
-        setMovies(data.movies);
+        setMovies(data?.movies);
+        
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
@@ -18,136 +22,90 @@ const Movielist = () => {
     fetchMovies();
   }, []);
 
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const itemsPerPage = 8;
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = movies?.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(movies?.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % movies?.length;
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div>
-      {/* When no movie present */}
-      <div className="no-movies">
-        <div>
-          <h1>Your movie list is empty</h1>
-          <button className="btn btnPrimary">Add a new movie</button>
-        </div>
-      </div>
-      {/* When no movie present */}
-
-      <div className="container">
-        <h2>Movies List</h2>
-        <ul>
-          {movies?.map((movie) => (
-            <li key={movie._id}>
-              {movie.title} - {movie.publishingYear}
-            </li>
-            // Add other movie details as needed
-          ))}
-        </ul>
-      </div>
-
-      {/* Movies list Page starts here */}
-      <div className="container">
-        <div className="page-heading">
-          <h1>
-            My movies
-            <a href="">
-              <img src="/add-white.svg" width={20} alt="" />
-            </a>
-          </h1>
-          <div>
-            <div className="logoutBtn">
-              <a className="text-white text-dec-none" href="">
-                <div>
-                  <span>Logout</span>
-                  <img className="ml-10" src="/logout-white.svg" alt="" />
-                </div>
+    <>
+      {movies?.length > 0 ? (
+        <div className="container">
+          <div className="page-heading">
+            <h1>
+              My movies
+              <a href="">
+                <img src="/add-white.svg" alt="" />
               </a>
+            </h1>
+            <div>
+              <div className="logoutBtn">
+                <a className="text-white text-dec-none" href="">
+                  <div>
+                    <span>Logout</span>
+                    <img className="ml-10 " src="/logout-white.svg" alt="" />
+                  </div>
+                </a>
+              </div>
             </div>
+          </div>
+
+          {/* movies card Loop should start from columns */}
+
+          <div className="row">
+            {currentItems &&
+              currentItems.map((movie) => (
+                <div className="col-md-3" key={movie._id}>
+                  <div className="cards">
+                    <Image
+                      src="/movie-poster.png"
+                      alt=""
+                      width={100}
+                      height={100}
+                    />
+                    <div className="cards_titles">
+                      <div className="cards_titles_heading">{movie?.title}</div>
+                      <div className="cards_titles_subheading">
+                        {movie?.publishingYear}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="Next"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={2}
+            pageCount={pageCount}
+            previousLabel="Prev"
+            renderOnZeroPageCount={null}
+            className="movie-paginate"
+          />
+          {/* Pagination code should run here */}
+        </div>
+      ) : (
+        <div className="no-movies">
+          <div>
+            <h1>Your movie list is empty</h1>
+            <button className="btn btnPrimary">Add a new movie</button>
           </div>
         </div>
-
-        {/* movies card Loop should start from columns */}
-        <div className="row">
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="cards">
-              <img src="/movie-poster.png" alt="" />
-              <div className="cards_titles">
-                <div className="cards_titles_heading">Movie 1</div>
-                <div className="cards_titles_subheading">2021</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* movies card */}
-
-        {/* Pagination code should run here */}
-        <br />
-        <br />
-        <div className="text-white text-center">1 2 3</div>
-        <br />
-        <br />
-        {/* Pagination code should run here */}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
